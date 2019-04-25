769022ff2a388a79ab3186bdbb890aa6d3137113
Nishkal <kashyapnishkal@gmail.com>
2019-04-25 11:11:18 +0530

    updated vuepress script

--- scripts/vuepress.ts
+++ scripts/vuepress.ts
@@ -1,6 +1,26 @@
 // import * as vm from 'vm';
+import * as fs from 'fs-extra';
 import * as press from 'vuepress';
-console.log(press.build);
+import * as mock from 'mock-require';
+
+// mock('fs-extra', function (str) {
+//     console.log(str);
+//     return fs;
+// });
+const fss = new Proxy(fs, {
+    get: (tsrget, key) => {
+        console.log(key);
+        return fs[key];
+    }
+})
+
+mock('fs-extra', fss);
+
+press.build('./docs').then((result) => {
+    console.log(result);
+}).catch((err) => {
+    console.log(err);
+});
 
 // const sandbox = { x: 2 };
 // vm.createContext(sandbox);
