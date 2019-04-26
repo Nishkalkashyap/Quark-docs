a0456f56c9354e0ff1cea5606a73c4b1e7842baf
Nishkal <kashyapnishkal@gmail.com>
2019-04-26 13:30:10 +0530

    added build notification alerts

---
+++ scripts/send-build-notification.ts
@@ -0,0 +1,24 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
+import * as web from '@slack/web-api';
+import * as functions from 'firebase-functions';
+import * as fs from 'fs-extra';
+
+type channels = 'storage' | 'general' | 'compute' | 'billing' | 'download' | 'documentation';
+
+const token = (process.env.BOT_ACCESS_TOKEN || (functions.config().slack || {}).bot);
+const client = new web.WebClient(token);
+function postMessage(channel: channels, text: string) {
+    client.chat.postMessage({
+        channel,
+        text,
+        mrkdwn: true
+    }).then((value) => {
+        console.log(text);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+
+const version = fs.readJsonSync('./package.json').version;
+postMessage('documentation', `Build version:${version} was uploaded.`);
