478fa5e67a3d5bbe94da1d56101cdd6059a807aa
Nishkal <kashyapnishkal@gmail.com>
2019-04-26 13:43:05 +0530

    minor changes

--- scripts/send-build-notification.ts
+++ scripts/send-build-notification.ts
@@ -1,12 +1,12 @@
 import * as dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
+
 import * as web from '@slack/web-api';
-import * as functions from 'firebase-functions';
 import * as fs from 'fs-extra';
 
 type channels = 'storage' | 'general' | 'compute' | 'billing' | 'download' | 'documentation';
 
-const token = (process.env.BOT_ACCESS_TOKEN || (functions.config().slack || {}).bot);
+const token = (process.env.BOT_ACCESS_TOKEN);
 const client = new web.WebClient(token);
 function postMessage(channel: channels, text: string) {
     client.chat.postMessage({
