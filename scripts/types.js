014aa498e25ff7d24c2889c238f871dec2a76f8f
Nishkal <kashyapnishkal@gmail.com>
2019-05-15 19:13:56 +0530

    added description in types

--- scripts/types.js
+++ scripts/types.js
@@ -1,18 +1,22 @@
 export const AllTags = {
     javascript: {
         color: 'var(--text-color--dark)',
-        background: 'var(--ion-color-warning)'
+        background: 'var(--ion-color-warning)',
+        description: `JavaScript (JS) is a lightweight interpreted or just-in-time compiled programming language.`
     },
     typescript: {
         color: 'var(--text-color--dark)',
-        background: 'var(--ion-color-primary)'
+        background: 'var(--ion-color-primary)',
+        description: `TypeScript is an open-source programming language developed and maintained by Microsoft. It is a strict syntactical superset of JavaScript.`
     },
     nodejs: {
         color: 'var(--ion-color-success)',
-        background: 'var(--text-color--dark)'
+        background: 'var(--text-color--dark)',
+        description: `Node.js is an open-source, cross-platform JavaScript run-time environment that executes JavaScript code outside of a browser.`
     },
     arduino: {
         color: 'var(--ion-color-secondary)',
-        background: 'var(--text-color--dark)'
+        background: 'var(--text-color--dark)',
+        description: `Arduino is an open-source hardware and software company, project and user community that designs and manufactures single-board microcontrollers.`
     }
 };
