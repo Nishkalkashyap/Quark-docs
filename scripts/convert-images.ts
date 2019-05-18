90193d54fa1bd84bda7ce2517b174145a8391fbc
Nishkal <kashyapnishkal@gmail.com>
2019-05-18 12:17:55 +0530

    updated convert images script

--- scripts/convert-images.ts
+++ scripts/convert-images.ts
@@ -3,49 +3,58 @@ import * as Path from 'path';
 import * as fs from 'fs-extra';
 import * as recc from 'recursive-readdir';
 
-const GIMAGES_PATH = './.vuepress/buildAssets';
-convertImages().catch(console.error);
+const ignoreFunction = (file: string, stat: fs.Stats) => {
+    if (file.endsWith('.png')) {
+        return false;
+    }
+    if (stat.isDirectory()) {
+        return false;
+    }
+    return true;
+}
 
-async function convertImages() {
-    const files = await recc(GIMAGES_PATH, [(file, stat) => {
-        if (file.endsWith('.png')) {
-            return false;
+const arr: IMap[] = [
+    {
+        inputPath: Path.join('./.vuepress/', './buildAssets/g-images'),
+        outputPath: Path.join('./.vuepress/', './public/g-images'),
+    }
+];
+
+convert(arr);
+
+function convert(maps: IMap[]) {
+    maps.map((map) => {
+
+        if (fs.statSync(map.inputPath).isDirectory()) {
+            convertFolder(map);
+            return;
         }
-        if (stat.isDirectory()) {
-            return false;
-        }
-        return true;
-    }]);
-
-    files.map(async (file) => {
-        const outFilePath = file.replace('buildAssets', 'public');
-        getConfigForPath(file);
-
-        await fs.ensureDir(Path.dirname(outFilePath));
-        const buffer = await sharp(file)
-            .resize(600)
-            .toBuffer();
-
-        await fs.writeFile(outFilePath, buffer);
+        convertFile(map);
     });
 }
 
-function getConfigForPath(path: string) {
-    const convertImageMapConfig: IMap[] = [{
-        regExp: /g-images/,
-    }];
-
-    const matches = convertImageMapConfig.map((val) => {
-        return path.match(val.regExp);
-    }).filter(val => !!val);
-
-    const bestMatch = matches.sort((a, b) => { return a.input.length - b.input.length });
-    // return bestMatch[0].in;
+async function convertFolder(map: IMap) {
+    const files = await recc(map.inputPath, [ignoreFunction]);
+    files.map(async (file) => {
+        const newMap = JSON.parse(JSON.stringify(map)) as IMap;
+        newMap.inputPath = file;
+        newMap.outputPath = Path.join(map.outputPath, Path.basename(file));
+        convertFile(newMap);
+    });
 }
 
+async function convertFile(map: IMap) {
+    const buffer = await sharp(map.inputPath)
+        .resize(map.width || 600, map.height)
+        .toBuffer();
+
+    fs.ensureDirSync(Path.dirname(map.outputPath));
+    await fs.writeFile(map.outputPath, buffer);
+}
 
 interface IMap {
-    regExp: RegExp;
+    inputPath: string;
+    outputPath: string;
     width?: number;
     height?: number;
 }
