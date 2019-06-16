f56b536d604e6a1b4421811a268021ee2b07d058
Nishkal <kashyapnishkal@gmail.com>
2019-06-16 21:45:47 +0530

    minor changes

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -7,23 +7,5 @@ export default ({
     // siteData // site metadata
 }) => {
     Vue.config.ignoredElements = ['ion-icon', 'ion-button']
-    console.log(router);
     router.addRoutes(json);
-    // router.beforeEach((to, from, next) => {
-    //     if (to.matched[0].path == '*') {
-    //         if (to.fullPath.includes('#')) {
-    //             const matchStr = to.path.replace(/[\/]$/, '') + '.html' + to.hash;
-    //             const match = router.match(matchStr);
-    //             const matchedPath = match.matched[0].path;
-    //             if (matchedPath !== '*') {
-    //                 next({ path: matchedPath });
-    //                 return;
-    //             }
-    //         }
-    //         next({ component: router.options.routes.reverse()[0].component });
-    //     } else {
-    //         next({ path: to.matched[0].path });
-    //         // next({ path: to.matched[0].path});
-    //     }
-    // });
 }
