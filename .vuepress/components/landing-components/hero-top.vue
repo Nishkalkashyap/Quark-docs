cacef023118572b75d8cf2963faabae01ef4727b
Nishkal <kashyapnishkal@gmail.com>
2019-06-30 00:19:57 +0530

    minor changes

--- .vuepress/components/landing-components/hero-top.vue
+++ .vuepress/components/landing-components/hero-top.vue
@@ -5,7 +5,7 @@
       <h1>Lightning fast app creation.</h1>
       <p
         class="description"
-      >Ionic Studio is a powerful app builder that delivers a smooth developer experience for teams building with Ionic. Get started with the fastest and easiest way to create award-winning cross-platform apps, from a single tool.</p>
+      >Quark Studio is a powerful app builder that delivers a smooth developer experience for teams building with Ionic. Get started with the fastest and easiest way to create award-winning cross-platform apps, from a single tool.</p>
       <p class="action">
         <router-link to="/guide/intro.html" class="nav-link action-button">Get Started →</router-link>
       </p>
