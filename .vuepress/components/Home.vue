c526143c33cf82c8ce6515ee4e102b37601d0c97
Nishkal <kashyapnishkal@gmail.com>
2019-07-01 10:20:57 +0530

    minor changes

--- .vuepress/components/Home.vue
+++ .vuepress/components/Home.vue
@@ -2,25 +2,6 @@
 <div class="home4-component">
     <heroTop />
 
-    <!-- <div class="full-height first-container landing-page-background">
-        <div class="first-container-wrapper flex-center">
-            <div class="image-container">
-                <video
-            muted
-            autoplay
-            loop
-            name="media"
-            poster="~@buildAssets/guide/hello-world-intro.png"
-            crossorigin="anonymous"
-            class="image-transition"
-          >
-            <source src="~@buildAssets/guide/showcase/web-browser.mp4" type="video/mp4">Your browser does not support the video tag.
-          </video>
-            </div>
-            <heroDownload />
-        </div>
-    </div>-->
-
     <heroSection side="left" heading="GitHub for Atom" upper="A text editor is at the core of a developer’s toolbox, but it doesn't usually work alone. Work with Git and GitHub directly from Atom with the GitHub package." lower="A text editor is at the core of a developer’s toolbox, but it doesn't usually work alone. Work with Git and GitHub directly from Atom with the GitHub package.">
         <video
         muted
@@ -73,10 +54,6 @@
 
     <div class="landing-page-background downloads-container">
         <floaters side="right" />
-        <!-- <svg viewBox="0 0 30 44.58" class="download-svg">
-        <path d="M28.74,20.81H1.26a1.26,1.26,0,0,1-1-2L14.16.5a1.25,1.25,0,0,1,1-.5h0a1.24,1.24,0,0,1,1,.51L29.75,18.8a1.25,1.25,0,0,1-1,2ZM3.81,18.29H26.22L15.16,3.37Z" />
-        <path d="M28.74,42H1.26a1.28,1.28,0,0,1-1.13-.71A1.26,1.26,0,0,1,.26,40l13.9-18.29a1.28,1.28,0,0,1,1-.5h0a1.24,1.24,0,0,1,1,.51L29.75,40a1.26,1.26,0,0,1,.12,1.32A1.28,1.28,0,0,1,28.74,42ZM3.81,39.47H26.22L15.16,24.55Z" />
-      </svg>-->
         <h1>All Downloads</h1>
         <Download disable_post_content="true" channel="stable" />
         <h3>
@@ -158,7 +135,6 @@ export default {
 
 .image-transition {
     transition: filter 2s;
-    // filter: brightness(0.75) contrast(1) sepia(1) hue-rotate(190deg) saturate(5);
     filter: brightness(0.7) contrast(1) sepia(1) hue-rotate(140deg) saturate(1.5);
     box-shadow: 0 20px 30px rgba(0, 0, 0, 0.5);
     border-radius: 5px;
@@ -169,7 +145,6 @@ export default {
 }
 
 .first-container {
-    // width: 1040px;
     overflow: hidden;
 
     div.image-container {
@@ -191,8 +166,6 @@ export default {
     position: relative;
 
     h1 {
-        // margin: 0px;
-        // background-color: var(--text-color--dark);
         padding: 15px 35px;
         border-radius: 4px;
         text-align: center;
@@ -200,9 +173,6 @@ export default {
         position: relative;
         margin-bottom: 80px !important;
         box-shadow: 0 20px 30px rgba(0, 0, 0, 0.5);
-        // border-top-right-radius: 20px;
-        // border-bottom-left-radius: 20px;
-        // border-top: solid 10px var(--accent-color);
     }
 
     @media only screen and (max-width: 600px) {
@@ -212,29 +182,13 @@ export default {
     }
 
     svg {
-        // height: 3em;
-        // position: relative;
-        // left: -160px;
-        // top: 5.5em;
         transform: rotate(180deg);
         bottom: 0vh;
         position: absolute;
         fill: #6574cd99;
         stroke: #6574cd99;
         z-index: -2;
-        // stroke-width: 2 !important;
     }
-
-    // svg {
-    //     // height: 50px;
-    //     height: 3em;
-    //     position: relative;
-    //     left: -160px;
-    //     // top: 90px;
-    //     top: 5.5em;
-    //     transform: rotate(180deg);
-    //     stroke-width: 2 !important;
-    // }
 
     h3 {
         border-radius: 2px;
