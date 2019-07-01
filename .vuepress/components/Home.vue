b399ef796a55fb68a75ff6b00a9c08a436030d19
Nishkal <kashyapnishkal@gmail.com>
2019-07-01 10:17:15 +0530

    deleted old components

--- .vuepress/components/Home.vue
+++ .vuepress/components/Home.vue
@@ -1,158 +1,246 @@
 <template>
-  <div class="background">
-    <canvas id="scene"></canvas>
-  </div>
+<div class="home4-component">
+    <heroTop />
+
+    <!-- <div class="full-height first-container landing-page-background">
+        <div class="first-container-wrapper flex-center">
+            <div class="image-container">
+                <video
+            muted
+            autoplay
+            loop
+            name="media"
+            poster="~@buildAssets/guide/hello-world-intro.png"
+            crossorigin="anonymous"
+            class="image-transition"
+          >
+            <source src="~@buildAssets/guide/showcase/web-browser.mp4" type="video/mp4">Your browser does not support the video tag.
+          </video>
+            </div>
+            <heroDownload />
+        </div>
+    </div>-->
+
+    <heroSection side="left" heading="GitHub for Atom" upper="A text editor is at the core of a developer’s toolbox, but it doesn't usually work alone. Work with Git and GitHub directly from Atom with the GitHub package." lower="A text editor is at the core of a developer’s toolbox, but it doesn't usually work alone. Work with Git and GitHub directly from Atom with the GitHub package.">
+        <video
+        muted
+        autoplay
+        loop
+        name="media"
+        poster="~@buildAssets/guide/hello-world-intro.png"
+        crossorigin="anonymous"
+        class="image-transition"
+        slot="image"
+      >
+        <source src="~@buildAssets/guide/showcase/web-browser.mp4" type="video/mp4" />Your browser does not support the video tag.
+      </video>
+    </heroSection>
+
+    <heroSection side="right" heading="GitHub for Atom" upper="A text editor is at the core of a developer’s toolbox, but it doesn't usually work alone. Work with Git and GitHub directly from Atom with the GitHub package." lower="A text editor is at the core of a developer’s toolbox, but it doesn't usually work alone. Work with Git and GitHub directly from Atom with the GitHub package.">
+        <video
+        muted
+        autoplay
+        loop
+        name="media"
+        poster="~@buildAssets/guide/hello-world-intro.png"
+        crossorigin="anonymous"
+        class="image-transition"
+        slot="image"
+      >
+        <source src="~@buildAssets/guide/hello-world-intro.mp4" type="video/mp4" />Your browser does not support the video tag.
+      </video>
+    </heroSection>
+
+    <heroSection side="left" heading="GitHub for Atom" upper="A text editor is at the core of a developer’s toolbox, but it doesn't usually work alone. Work with Git and GitHub directly from Atom with the GitHub package." lower="A text editor is at the core of a developer’s toolbox, but it doesn't usually work alone. Work with Git and GitHub directly from Atom with the GitHub package.">
+        <video
+        muted
+        autoplay
+        loop
+        name="media"
+        poster="~@buildAssets/guide/hello-world-intro.png"
+        crossorigin="anonymous"
+        class="image-transition"
+        slot="image"
+      >
+        <source src="~@buildAssets/guide/showcase/web-browser.mp4" type="video/mp4" />Your browser does not support the video tag.
+      </video>
+    </heroSection>
+
+    <div class="landing-page-background">
+        <floaters side="left" />
+        <heroFeatures />
+    </div>
+
+    <div class="landing-page-background downloads-container">
+        <floaters side="right" />
+        <!-- <svg viewBox="0 0 30 44.58" class="download-svg">
+        <path d="M28.74,20.81H1.26a1.26,1.26,0,0,1-1-2L14.16.5a1.25,1.25,0,0,1,1-.5h0a1.24,1.24,0,0,1,1,.51L29.75,18.8a1.25,1.25,0,0,1-1,2ZM3.81,18.29H26.22L15.16,3.37Z" />
+        <path d="M28.74,42H1.26a1.28,1.28,0,0,1-1.13-.71A1.26,1.26,0,0,1,.26,40l13.9-18.29a1.28,1.28,0,0,1,1-.5h0a1.24,1.24,0,0,1,1,.51L29.75,40a1.26,1.26,0,0,1,.12,1.32A1.28,1.28,0,0,1,28.74,42ZM3.81,39.47H26.22L15.16,24.55Z" />
+      </svg>-->
+        <h1>All Downloads</h1>
+        <Download disable_post_content="true" channel="stable" />
+        <h3>
+            <router-link to="/download/insiders">Or get the insiders release</router-link>
+        </h3>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 338.05" preserveAspectRatio="none">
+            <path>
+              <animate
+                attributeName="d"
+                values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z"
+                repeatCount="indefinite"
+                dur="30s"
+              />
+            </path>
+          </svg>
+    </div>
+
+    <SWUpdatePopup :updateEvent="swUpdateEvent" />
+</div>
 </template>
 
 <script>
+import companyLogos from "./landing-components/companyLogos";
+import heroDownload from "./landing-components/hero-download2";
+import heroSection from "./landing-components/hero-section";
+import heroTop from "./landing-components/hero-top";
+import Download from "./Download";
+import SWUpdatePopup from "@default-theme/SWUpdatePopup.vue";
+import floaters from "./landing-components/floaters";
+import heroFeatures from "./landing-components/hero-features";
 export default {
-  mounted: function() {
-    var canvas = document.querySelector("#scene"),
-      ctx = canvas.getContext("2d"),
-      particles = [],
-      amount = 0,
-      mouse = { x: 0, y: 0 },
-      radius = 1;
-
-    // var colors = ["#468966", "#FFF0A5", "#FFB03B", "#B64926", "#8E2800"];
-    var colors = ["#3880ff", "#222428", "#989aa2", "#000000", "#000000"];
-
-    var ww = (canvas.width = window.innerWidth);
-    var wh = (canvas.height = window.innerHeight);
-
-    function Particle(x, y) {
-      this.x = Math.random() * ww;
-      this.y = Math.random() * wh;
-      this.dest = {
-        x: x,
-        y: y
-      };
-      this.r = Math.random() * 5 + 2;
-      this.vx = (Math.random() - 0.5) * 20;
-      this.vy = (Math.random() - 0.5) * 20;
-      this.accX = 0;
-      this.accY = 0;
-      this.friction = Math.random() * 0.05 + 0.94;
-
-      this.color = colors[Math.floor(Math.random() * 6)];
-    }
-
-    Particle.prototype.render = function() {
-      this.accX = (this.dest.x - this.x) / 1000;
-      this.accY = (this.dest.y - this.y) / 1000;
-      this.vx += this.accX;
-      this.vy += this.accY;
-      this.vx *= this.friction;
-      this.vy *= this.friction;
-
-      this.x += this.vx;
-      this.y += this.vy;
-
-      ctx.fillStyle = this.color;
-      ctx.strokeStyle = this.color;
-      // ctx.beginPath();
-      // ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
-      // ctx.fill();
-
-      ctx.beginPath();
-      // ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
-      ctx.moveTo(this.x, this.y + 3);
-      ctx.lineTo(this.x, this.y);
-      ctx.lineTo(this.x + 3, this.y + 3);
-      ctx.fill();
-      ctx.stroke();
-
-      var a = this.x - mouse.x;
-      var b = this.y - mouse.y;
-
-      var distance = Math.sqrt(a * a + b * b);
-      if (distance < radius * 70) {
-        this.accX = (this.x - mouse.x) / 100;
-        this.accY = (this.y - mouse.y) / 100;
-        this.vx += this.accX;
-        this.vy += this.accY;
-      }
-    };
-
-    function onMouseMove(e) {
-      mouse.x = e.clientX;
-      mouse.y = e.clientY;
-    }
-
-    function onTouchMove(e) {
-      if (e.touches.length > 0) {
-        mouse.x = e.touches[0].clientX;
-        mouse.y = e.touches[0].clientY;
-      }
-    }
-
-    function onTouchEnd(e) {
-      mouse.x = -9999;
-      mouse.y = -9999;
-    }
-
-    function initScene() {
-      ww = canvas.width = window.innerWidth;
-      wh = canvas.height = window.innerHeight;
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      //   ctx.font = "bold " + ww / 10 + "px sans-serif";
-      // ctx.font = "200px Georgia";
-      ctx.font = "200px Eina";
-      ctx.textAlign = "center";
-      ctx.fillText("Quark", ww / 2, wh / 2);
-
-      var data = ctx.getImageData(0, 0, ww, wh).data;
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.globalCompositeOperation = "screen";
-
-      particles = [];
-      for (var i = 0; i < ww; i += Math.round(ww / 150)) {
-        for (var j = 0; j < wh; j += Math.round(ww / 150)) {
-          if (data[(i + j * ww) * 4 + 3] > 150) {
-            particles.push(new Particle(i, j));
-          }
+    components: {
+        SWUpdatePopup,
+        companyLogos,
+        heroDownload,
+        heroSection,
+        heroTop,
+        floaters,
+        heroFeatures
+    },
+    data: () => {
+        return {
+            swUpdateEvent: null
+        };
+    },
+    mounted() {
+        this.$on("sw-updated", this.onSWUpdated);
+    },
+    methods: {
+        onSWUpdated(e) {
+            console.log(`Service Worker Updated`);
+            this.swUpdateEvent = e;
         }
-      }
-      amount = particles.length;
     }
-
-    function onMouseClick() {
-      radius++;
-      if (radius === 5) {
-        radius = 0;
-      }
-    }
-
-    function render(a) {
-      requestAnimationFrame(render);
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      for (var i = 0; i < amount; i++) {
-        particles[i].render();
-      }
-    }
-
-    // copy.addEventListener("keyup", initScene);
-    window.addEventListener("resize", initScene);
-    window.addEventListener("mousemove", onMouseMove);
-    window.addEventListener("touchmove", onTouchMove);
-    window.addEventListener("click", onMouseClick);
-    window.addEventListener("touchend", onTouchEnd);
-    initScene();
-    requestAnimationFrame(render);
-  }
 };
 </script>
 
 <style lang="scss" scoped>
-div.background {
-  position: absolute;
-  top: 0px;
+.full-height {
+    height: 100vh;
+    width: 100%;
+    max-width: 100%;
 }
 
-canvas {
-  background: transparent;
-  width: 100% !important;
+.flex-center {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    padding: 0px 50px;
+    box-sizing: border-box;
+}
+
+.first-container-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+}
+
+.image-transition {
+    transition: filter 2s;
+    // filter: brightness(0.75) contrast(1) sepia(1) hue-rotate(190deg) saturate(5);
+    filter: brightness(0.7) contrast(1) sepia(1) hue-rotate(140deg) saturate(1.5);
+    box-shadow: 0 20px 30px rgba(0, 0, 0, 0.5);
+    border-radius: 5px;
+}
+
+.image-transition:hover {
+    filter: brightness(1) contrast(1) sepia(0) hue-rotate(0deg) saturate(1);
+}
+
+.first-container {
+    // width: 1040px;
+    overflow: hidden;
+
+    div.image-container {
+        width: 100%;
+        display: flex;
+        align-items: center;
+
+        video {
+            max-width: 100%;
+        }
+    }
+
+    div.image-container:hover {
+        filter: brightness(1) contrast(1) sepia(0) hue-rotate(0deg) saturate(1);
+    }
+}
+
+.downloads-container {
+    position: relative;
+
+    h1 {
+        // margin: 0px;
+        // background-color: var(--text-color--dark);
+        padding: 15px 35px;
+        border-radius: 4px;
+        text-align: center;
+        top: 70px;
+        position: relative;
+        margin-bottom: 80px !important;
+        box-shadow: 0 20px 30px rgba(0, 0, 0, 0.5);
+        // border-top-right-radius: 20px;
+        // border-bottom-left-radius: 20px;
+        // border-top: solid 10px var(--accent-color);
+    }
+
+    @media only screen and (max-width: 600px) {
+        h1 {
+            font-size: 36px !important;
+        }
+    }
+
+    svg {
+        // height: 3em;
+        // position: relative;
+        // left: -160px;
+        // top: 5.5em;
+        transform: rotate(180deg);
+        bottom: 0vh;
+        position: absolute;
+        fill: #6574cd99;
+        stroke: #6574cd99;
+        z-index: -2;
+        // stroke-width: 2 !important;
+    }
+
+    // svg {
+    //     // height: 50px;
+    //     height: 3em;
+    //     position: relative;
+    //     left: -160px;
+    //     // top: 90px;
+    //     top: 5.5em;
+    //     transform: rotate(180deg);
+    //     stroke-width: 2 !important;
+    // }
+
+    h3 {
+        border-radius: 2px;
+        margin-top: 60px;
+        margin-bottom: 50px;
+        padding: 10px 20px;
+    }
 }
 </style>
