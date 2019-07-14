179a3e92525674741c7ba00afa104a851c79f4e9
Nishkal <kashyapnishkal@gmail.com>
2019-07-14 19:15:39 +0530

    added cache-control

--- docs/scripts/cloud-storage-hosting.ts
+++ docs/scripts/cloud-storage-hosting.ts
@@ -18,26 +18,46 @@ async function uploadFileToBucket() {
     const storage = new Storage({
         projectId: 'diy-mechatronics'
     });
+
     const bucket = storage.bucket(bucketName);
-
     await bucket.deleteFiles();
-
+    
     const files = await recc('./');
     const promises = files.map(async (_file) => {
         const file = _file.replace(/\\/g, '/');
+        // console.log(`File: ${file}; ${getCacheControlForFile(file)}`);
         const f = await bucket.upload(file, {
             gzip: true,
             public: true,
             destination: file,
-            // metadata: {
-            //     // cacheControl: 'public, max-age=31536000',
-            // }
+            metadata: {
+                cacheControl: `public, max-age=${getCacheControlForFile(file)}`,
+            }
         });
     });
     await Promise.all(promises);
     printConsoleStatus(`Uploaded all files`, 'success');
 }
 
-// function getCacheControlForFile(file : string){
-//     if(file.match(/\.(jpg|jpeg|gif|png|mp4)$/))
-// }
+function getCacheControlForFile(file: string) {
+    // html max-age 3600
+    // html max-age 3600
+    
+    if (file.match(/\.(jpg|jpeg|gif|png|mp4)$/)) {
+        return 2592000;
+    }
+
+    if (file.match(/assets\/js.+(js)$/)) {
+        return 2592000;
+    }
+
+    if (file.match(/\.(woff|woff2|css)$/)) {
+        return 2592000;
+    }
+
+    if (file.match(/\.+worker.js/)) {
+        return 2592000;
+    }
+
+    return 3600;
+}
