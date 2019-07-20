aa9666914e79020e0f413304aec36be91c8892b7
Nishkal <kashyapnishkal@gmail.com>
2019-07-20 16:54:57 +0530

    added imgur

--- docs/scripts/sw.js
+++ docs/scripts/sw.js
@@ -35,10 +35,46 @@ workbox.routing.registerRoute(
     })
 );
 
+// imgur images
+workbox.routing.registerRoute(
+    /^https:.*imgur.*\.(png|jpg|jpeg|gif)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'imgur-images',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [200],
+            }),
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 7,
+                maxEntries: 30,
+            })
+        ]
+    })
+);
+
+// imgur videos
+workbox.routing.registerRoute(
+    /^https:.*imgur.*\.(mp4)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'imgur-videos',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [200],
+            }),
+            new workbox.rangeRequests.Plugin(),
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 7,
+                maxEntries: 30,
+            })
+        ]
+    })
+);
+
 // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#cached-av
 workbox.routing.registerRoute(
     // new RegExp('.(mp4)$'),
-    /.*\.mp4/,//
+    // /.*\.mp4/,//
+    /.*quarkjs.*\.mp4/,//
     new workbox.strategies.CacheFirst({
         cacheName: 'videos',
         plugins: [
