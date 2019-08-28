ccdf606a6464496a2921b5127728daabd1ce164d
Nishkal <kashyapnishkal@gmail.com>
2019-08-28 14:05:00 +0530

    minor changes

--- docs/.vuepress/components/SocialIcons.vue
+++ docs/.vuepress/components/SocialIcons.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="social-icon-container">
-    <a class="social-icon" v-for="item of social" v-html="item.svg" :href="item.link" target="_blank"></a>
+    <a class="social-icon" :title="item.title" v-for="item of social" v-html="item.svg" :href="item.link" target="_blank"></a>
 </div>
 </template>
 
@@ -9,24 +9,29 @@ export default {
     data() {
         return {
             social: [{
-                    svg: require("!!svg-inline-loader!@buildAssets/signature/instagram.svg"),
-                    link: "https://www.instagram.com/nishkalkashyap/"
+                    svg: require("!!svg-inline-loader!@buildAssets/signature/github.svg"),
+                    link: "https://github.com/nishkalkashyap",
+                    title : 'GitHub'
                 },
                 {
                     svg: require("!!svg-inline-loader!@buildAssets/signature/twitter.svg"),
-                    link: "https://twitter.com/nishkalkashyap"
-                },
-                {
-                    svg: require("!!svg-inline-loader!@buildAssets/signature/github.svg"),
-                    link: "https://github.com/nishkalkashyap"
+                    link: "https://twitter.com/nishkalkashyap",
+                    title : 'Twitter'
                 },
                 {
                     svg: require("!!svg-inline-loader!@buildAssets/signature/website.svg"),
-                    link: "https://nishkal.in"
+                    link: "https://nishkal.in",
+                    title : 'Website'
                 },
                 {
                     svg: require("!!svg-inline-loader!@buildAssets/signature/mail.svg"),
-                    link: "mailto:hello@nishkal.in?subject=Hey%20Nishkal,"
+                    link: "mailto:hello@nishkal.in?subject=Hey%20Nishkal,",
+                    title : 'Email'
+                },
+                {
+                    svg: require("!!svg-inline-loader!@buildAssets/signature/instagram.svg"),
+                    link: "https://www.instagram.com/nishkalkashyap/",
+                    title : 'Instagram'
                 }
             ]
         };
