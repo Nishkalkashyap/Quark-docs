155ff79e987309563c0e9ef4e5d49c6ffd7559d9
Nishkal <kashyapnishkal@gmail.com>
2019-08-30 21:09:24 +0530

    added analytics.js

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,4 +1,5 @@
 import json from './routes.json';
+import addAnalytics from './analytics';
 
 export default ({
     Vue, // the version of Vue being used in the VuePress app
@@ -8,32 +9,4 @@ export default ({
 }) => {
     router.addRoutes(json);
     addAnalytics(router, 'UA-112064718-3');
-}
-
-function addAnalytics(router, GA_ID) {
-    if (process.env.NODE_ENV === 'production' && GA_ID && typeof window !== 'undefined') {
-        (function (i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r
-            i[r] = i[r] || function () {
-                (i[r].q = i[r].q || []).push(arguments)
-            }
-            i[r].l = 1 * new Date()
-            a = s.createElement(o)
-            m = s.getElementsByTagName(o)[0]
-            a.async = 1
-            a.src = g
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
-
-        ga('create', GA_ID, 'auto');
-        ga('set', 'anonymizeIp', true);
-
-        let previous;
-        router.afterEach(function (to) {
-            if (previous != to.path) {
-                previous = to.path;
-                ga('send', 'pageview', to.path);
-            }
-        });
-    }
 }
